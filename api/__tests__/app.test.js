4ba62768bac24962d5a7b4db491f60c309ea19a2
Mycenae1 <james-ziprin@outlook.com>
2023-01-16 15:09:22 +0000

    commiting task 3 GET topics

---
+++ api/__tests__/app.test.js
@@ -0,0 +1,54 @@
+const supertest = require('supertest');
+const db = require("../../db/connection");
+const seed = require('../../db/seeds/seed');
+const app = require('../app');
+const data= require('../../db/data/test-data')
+
+beforeAll(() =>{
+    return seed(data)
+});
+
+
+describe.only('GET api/topics', () => {
+    test('should return a 200 status', () => {
+        return supertest(app).get('/api/topics').expect(200)
+    
+    })
+    test('should return an array of topic objects with correct properties', () => {
+        return supertest(app).get('/api/topics').expect(200)
+        .then(({body}) => {
+            expect(body.length).toBeGreaterThan(0);
+
+            body.forEach((topic) => {
+                    expect(topic).toHaveProperty('slug');
+                    expect(topic).toHaveProperty('description');
+                  
+                   
+            })
+        })
+    })
+
+    test('should return 404 error when url is not found', () => {
+        return supertest(app)
+        .get('/api/banana')
+        .expect(404)
+        .then(({body}) => {
+            expect(body.message).toBe('URL not found');
+
+        })
+    })
+
+
+
+})
+
+
+
+
+
+
+
+
+afterAll(() =>{
+    db.end();
+});
